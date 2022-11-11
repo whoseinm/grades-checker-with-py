1885834cc8ab007b82f86d47adbc8aad9fd80dc5
whoseinm <huseynmikayilzade07@gmail.com>
2022-11-11 23:22:11 +0400

    exercise with len

---
+++ exercise_w_len.py
@@ -0,0 +1,4 @@
+a = ["Arzu 45623777", "Tahir 74569841", "İradə 32154578", "Səmra 45698712", "Yusif 72568713", "Nuray 11328702"]
+print("Siyahının uzunluğu:",len(a))
+print("Sonuncu elementin indeksi:",len(a)-1)
+print("Sonuncu elementin qiyməti:",a[5])
