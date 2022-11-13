1cd5de6c9dd5e0427391afb065dd2899df606645
whoseinm <huseynmikayilzade07@gmail.com>
2022-11-13 22:47:40 +0400

    Exercise with index

---
+++ ExerciseWithIndex.py
@@ -0,0 +1,7 @@
+a = ['Lətif 6 yanvar','Təranə 25 fevral','Cəlal 3 mart','Səlim 15 mart','Pərviz 26 mart','Kənan 8 aprel']
+print('Fevralda və martda: ')
+print(a[1:5])
+print('Martda: ') 
+print(a[2:5])
+print('Martda və apreldə: ')
+print(a[2:6])
