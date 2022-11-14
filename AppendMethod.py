b66ad57d26c343ce2f5dadb0cb28e520117bea06
whoseinm <huseynmikayilzade07@gmail.com>
2022-11-14 23:48:23 +0400

    append method in python

---
+++ AppendMethod.py
@@ -0,0 +1,7 @@
+shop =['çuğundur 12 ədəd','kök 8 ədəd','yumurta 30 ədəd','portağal 5 ədəd']
+print('Anbarda mövcud olan ərzaqlar:')
+print(shop)
+new = input('Ərzaq və miqdarını seçin')
+shop.append(new)
+print('Anbarda mövcud olan ərzaqlar:')
+print(shop)
