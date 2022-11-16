e946e0e54e608feca7267195a421336853cde0cd
whoseinm <huseynmikayilzade07@gmail.com>
2022-11-16 23:13:18 +0400

    Exercise with remove del and list

---
+++ exercise2.py
@@ -0,0 +1,7 @@
+shop =['çuğundur 12 ədəd','kök 8 ədəd','yumurta 30 ədəd','portağal 5 ədəd']
+print('Anbardakı ərzaqlar:')
+print(shop)
+print('Portağal qurtarıb! Onu bazadan silin.')
+del shop[3]
+print('Anbarda mövcud olan ərzaqlar:')
+print(shop)
