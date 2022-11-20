69bf2bbab7747bb9f08f7cb583eeabf0f02deaec
whoseinm <huseynmikayilzade07@gmail.com>
2022-11-20 23:57:31 +0400

    while

---
+++ while.py
@@ -0,0 +1,6 @@
+answer = input("Daha bir iştirakçı daxil edilsin?")
+while answer=="Bəli":
+    name = input("Ad daxil edin")
+    surname = input("Soyad daxil edin")
+    print("Soyad Ad: " + name + " " + surname + ".")
+    answer = input("Daha bir iştirakçı daxil edilsin?")
