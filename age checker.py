8cdab0a0076cd29ebba86464b17603ea5480d47c
whoseinm <huseynmikayilzade07@gmail.com>
2022-12-04 23:32:01 +0400

    age checker

---
+++ age checker.py
@@ -0,0 +1,8 @@
+age = [12, 15, 17, 16, 16, 15, 10, 17, 18, 13, 11]
+age.sort()
+print(age)
+min=age[0]
+max=age[len(age)-1]
+print("Ən böyük yaş",max)
+print("Ən kiçik yaş",min)
+print("Yaşlar arasında fərq", max-min)
