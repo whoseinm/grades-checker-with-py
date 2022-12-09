48613de05d8d9fae8b64a8182cd646fb7fc8371f
whoseinm <huseynmikayilzade07@gmail.com>
2022-12-09 23:22:48 +0400

    plus

---
+++ plus.py
@@ -0,0 +1,8 @@
+a = [5, 2, 4, 6, 3, 4, 5, 2, 3, 6]
+print('Səbətə qiymətləri ilə mallar əlavə edilib:')
+print(a)
+summ = 0
+for i in range(10):
+    summ = summ + a[i]
+print('Alış məbləği:')
+print(summ)
