7954b5acac346ee6f10e5bda33c59410111dbc6a
whoseinm <huseynmikayilzade07@gmail.com>
2023-01-30 23:10:34 +0400

    exam question

---
+++ abrevation.py
@@ -0,0 +1,8 @@
+name = ["Azərbaycan", "Braziliya", "Rusiya", "Fransa"]
+abbreviation = ["AZ", "BR", "RU", "FR"]
+dictionary = {}
+for i in range(4):
+    a = name[i]
+    b = abbreviation[i]
+    dictionary[a] = b
+print(dictionary)
