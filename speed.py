fe2e9e644c769a922d49b9781bfb29eb9f2b170e
whoseinm <huseynmikayilzade07@gmail.com>
2023-02-21 23:18:43 +0400

    speed calculator

---
+++ speed.py
@@ -0,0 +1,7 @@
+from math import*
+a = int(input("L uzunluğunu daxil edin"))
+b = int(input("H hündürlüyünü daxil edin"))
+c = int(input("t vaxtını daxil edin"))
+S = sqrt(pow(b,2)+ pow(a,2))
+V = S/c
+print("Sürət bərabərdir",V)
